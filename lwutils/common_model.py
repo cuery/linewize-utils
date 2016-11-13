7d8bb0790203bc9231526ddb683f261771a224e0
alex.mack@linewize.com <alex.mack@linewize.com>
2016-11-14 11:28:28 +1300

    indent fix

--- lwutils/common_model.py
+++ lwutils/common_model.py
@@ -5,8 +5,8 @@ class CloudUser:
     support_admin = False
     phone_number = ""
     enabled = True
-	msp = None
-	two_factor = False
+    msp = None
+    two_factor = False
     attributes = ["customerid", "email", "password", "support_admin", "phone_number", "enabled", "two_factor", "msp"]
 
     def __init__(self, customerid=None, email=None, password=None, enabled=True, phone_number=None, two_factor=False):
