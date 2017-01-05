a9b3446afcdcfb8f4e2f7b24d0d03b247c5e1548
Thorsten Castor <thorsten.castor@linewize.com>
2017-01-05 15:09:41 +1300

    added local queue client

---
+++ lwutils/queue/clients/local_queue_client.py
@@ -0,0 +1,47 @@
+from lwutils.mongo_queue import MongoQueue
+import time
+
+
+class LocalQueueClient(object):
+
+    def __init__(self, config):
+        self.config = config
+        self.queue_client = MongoQueue(self.config)
+
+    def get_queue(self, queue_name):
+        queue = LocalQueue(self.queue_client, queue_name)
+        return queue
+
+
+class LocalQueue(object):
+
+    def __init__(self, queue_client, local_queue):
+        self.queue_client = queue_client
+        self.local_queue = local_queue
+
+    def send_message(self, message_body):
+        self.queue_client.add(self.local_queue, message_body)
+
+    def receive_messages(self, max_number_of_messages, wait_time_seconds):
+        msgs = []
+        msg = self.queue_client.get(self.local_queue)
+        # mongo_queue support only single message retrieval atm
+        if msg:
+            msgs.append(LocalMessage(self.queue_client, self.local_queue, msg))
+        else:
+            time.sleep(1)  # so we don't run wild, mongo_queue doesn't support wait_time atm
+        return msgs
+
+
+class LocalMessage(object):
+
+    def __init__(self, queue_client, local_queue, msg):
+        self.queue_client = queue_client
+        self.local_queue = local_queue
+        self.msg = msg
+
+    def get_body(self):
+        return self.msg['msg']
+
+    def delete(self):
+        self.local_queue.delete(self.msg['id'])
--- lwutils/queue/queue_client.py
+++ lwutils/queue/queue_client.py
@@ -1,4 +1,5 @@
 from lwutils.queue.clients.aws_queue_client import AWSQueueClient
+from lwutils.queue.clients.local_queue_client import LocalQueueClient
 
 
 class QueueClient(object):
@@ -6,5 +7,7 @@ class QueueClient(object):
     def factory(config):
         if config.get('QUEUE_PROVIDER') == "AWS":
             return AWSQueueClient(config)
-        # assert 0, "Unknown Queue provider: " + config.get('QUEUE_PROVIDER', "not configured")
+        if config.get('QUEUE_PROVIDER') == "LOCAL":
+            return LocalQueueClient(config)
+        assert 0, "Unknown Queue provider: " + config.get('QUEUE_PROVIDER', "not configured")
     factory = staticmethod(factory)
