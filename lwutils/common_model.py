33e1fe8700174d7d5a5ed96c8173bdf56053241b
Thorsten Castor <thorsten.castor@linewize.com>
2017-11-23 16:08:37 +1300

    added mobilezone_enabled to CloudDevice model

--- lwutils/common_model.py
+++ lwutils/common_model.py
@@ -87,6 +87,7 @@ class CloudDevice:
     logging_configuration__upload_ttl = 0
     logging_configuration__flush_files = 0
     teacher_classrooms_enabled = False
+    mobilezone_enabled = False
 
     # If you add an attribute to this class, MAKE SURE YOU ADD IT TO THIS LIST
     attributes = ["deviceid", "description", "user_defined_name",
@@ -101,7 +102,7 @@ class CloudDevice:
                   "update_branch", "classroom_groups_group_prefix",
                   "watchdog_recipients", "xero_id", "msp", "classwize_reliever_enabled",
                   "logging_configuration__upload_ttl", "logging_configuration__flush_files", 
-                  "teacher_classrooms_enabled"]
+                  "teacher_classrooms_enabled", "mobilezone_enabled"]
 
     def __init__(self, deviceid=None, description=""):
         self.deviceid = deviceid
