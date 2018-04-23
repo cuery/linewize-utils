810e86ae1cf2bc5390d99b8b287266703d03de58
Thorsten Castor <thorsten.castor@linewize.com>
2018-04-23 17:48:19 +1200

    fixed async device config handler and added missing annotations

--- lwutils/device_config_handler_async.py
+++ lwutils/device_config_handler_async.py
@@ -2,6 +2,7 @@ import json
 import logging
 from lwutils.accountmanagementservice_client_async import AsyncAccountManagementPersistenceService
 from lwutils.erouter_client_async import AsyncERouterClient
+from tornado import gen
 
 
 class AsyncDeviceConfigHandler():
@@ -15,14 +16,16 @@ class AsyncDeviceConfigHandler():
         if engage:
             self.engage()
 
+    @gen.coroutine
     def engage(self):
         try:
-            self.erouter_client.subscribe("device_config_update", self.application_name, self.update_device_config_callback)
+            yield self.erouter_client.subscribe("device_config_update", self.application_name, self.update_device_config_callback)
             self.logger.info('Subscribe %s to device config updates', self.application_name)
         except:
             self.logger.exception("An error occurred while initing the event bus from %s.", (self.application_name))
-        self.update_all_devices()
+        yield self.update_all_devices()
 
+    @gen.coroutine
     def update_device_config_callback(self, data):
         if data:
             msg = json.loads(data)
@@ -31,6 +34,7 @@ class AsyncDeviceConfigHandler():
                 self.devices[msg['deviceid']] = device.to_dict()
                 self.logger.info('%s received new config for device %s' % (self.application_name, msg['deviceid']))
 
+    @gen.coroutine
     def update_all_devices(self):
         self.logger.info('Update all devices for %s' % self.application_name)
         devices = yield AsyncAccountManagementPersistenceService.get_all_devices(self.config["ACCOUNT_MANAGEMENT_SERVICE"])
